2bd3124717ff4277910cbf50f98edaf396c2861a
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-12-09 22:15:06 +0600

    Project start

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# National_Martyrs_Monument______animation
+# Hi , Welcome to my simple project.
 
 # Here i create an animation on the National Martyr's Monument of Bangladesh in the memory of the martyrs who died in the greate liberation war, 1971.
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include<graphics.h>
 using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    cout<<n;
+    cout << "Hello world!" << endl;
+    int gd,gm;
+    detectgraph(&gd,&gm);
+    initgraph(&gd,&gm,"c:\\TURBOC3\\BGI");
+    getchar();
     return 0;
 }
