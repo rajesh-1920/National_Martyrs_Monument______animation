097c2eb121f7b0209bf057c80f55f456d2a83b9a
Rajesh Biswas <rajesh18@cse.pstu.ac.bd>
2024-12-09 22:32:46 +0600

    line added

--- main.cpp
+++ main.cpp
@@ -2,12 +2,25 @@
 #include<graphics.h>
 using namespace std;
 
+//--------declaration---------------
+void baseline();
+//----------------------------------
+
 int main()
 {
     cout << "Hello world!" << endl;
     int gd,gm;
     detectgraph(&gd,&gm);
     initgraph(&gd,&gm,"c:\\TURBOC3\\BGI");
+
+    baseline();
+
     getchar();
     return 0;
+}
+
+void baseline()
+{
+    setcolor(YELLOW);
+    line(320,10,320,300);
 }
